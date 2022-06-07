50e95aed10aac5e14c1d70a8b20b06859760638a
Adr204 <70309639+Adr204@users.noreply.github.com>
2022-06-07 15:21:31 +0900

    一部コードのリファクタリングを行いました

--- js/content.js
+++ js/content.js
@@ -9,45 +9,61 @@
 
 (() => {
 	const movies = document.getElementsByClassName("movie");
-	const cat = {
-		watched: 0,
-		unwatched: 0,
-		supplement: 0
-	};
-	function timeFormat(time) {
-		const hour = Math.floor(time / 3600);
-		const minute = Math.floor(time / 60) % 60;
-		const second = time % 60;
+	class movieBox {
+		constructor() {
+			this.list = [];
+			this.sum = 0;
+		}
+		push(data) {
+			this.list.push(data);
+			this.sum += data.length;
+		}
+		format() {
+			const hour = Math.floor(this.sum / 3600);
+			const minute = Math.floor(this.sum / 60) % 60;
+			const second = this.sum % 60;
 	
-		return `${hour}時間 ${minute}分 ${second}秒`;
+			return ` ${hour}時間 ${minute}分 ${second}秒`
+		}
 	}
+	/** 
+	 * @todo カテゴリ分けせずに一つのList型に統合してみる
+	*/
+	const cate = {
+		watched: new movieBox(),
+		unwatched: new movieBox(),
+		supplement: new movieBox()
+	};
 
 	for (const movie of movies) {
-		const tag = movie.className;
-		const lenStr = movie.getElementsByClassName("movie-length")[0].textContent;
-		const lenArr = lenStr.split(':').reverse();
-		
-		let length = 0;
-		for(let i = 0; i < lenArr.length; i++) length += lenArr[i] * (60 ** i);
+		const tag = movie.classList;
+		const data = {
+			init: function(mov) {
+				const lenStr = mov.getElementsByClassName("movie-length")[0].textContent;
+				const lenArr = lenStr.split(':').map(e => Number(e));
+				this.length = lenArr.reverse().reduce((prev, current, index) => prev + current * 60 ** index);
+				this.title = mov.getElementsByClassName("title")[0].textContent;
+				// supplementは常に閉じてるっぽい
+				// なんで?
+				this.isClosed = mov.childNodes[0].classList.contains("is-gate-closed");
+			}
+		}
+		data.init(movie);
+		// console.log(data);
 
-		switch (tag) {
-			case "movie good":
-				cat.watched += length;
-				break;
-			case "movie":
-				cat.unwatched += length;
-				break;
-			case "movie supplement":
-			case "movie good supplement":
-				cat.supplement += length;
-				break;
-			default:
-				console.error(`不明な種類の動画が含まれています\n${tag}`);
-				break;
+		if(tag.contains("supplement")) {
+			cate.supplement.push(data);
+		} else if(tag.contains("good")) {
+			cate.watched.push(data);
+		} else {
+			cate.unwatched.push(data);
 		}
 	}
 
-	if(cat.unwatched) Snackbar.show({text: `このページの動画は ${timeFormat(cat.unwatched)}が未視聴です`});
+	// console.log(cate.watched);
+	// console.log(cate.unwatched);
+	// console.log(cate.supplement);
+	if(cate.unwatched.sum) Snackbar.show({text: `このページの動画は ${cate.unwatched.format()}が未視聴です`});
 	else Snackbar.show({text:`このページの動画は全て視聴済みです`});
-	return cat;
+	return cate;
 })();
